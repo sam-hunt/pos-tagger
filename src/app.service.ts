92b9589c3fb68e570b1dd279f61f1af0bbd95a47
Sam Hunt <s2112201@ipc.ac.nz>
2019-04-08 00:57:30 +1200

    docs: edit readme and package.json for v1

--- src/app.service.ts
+++ src/app.service.ts
@@ -5,10 +5,11 @@ export class AppService {
     getHello(): string {
         return `
             <h1>Welcome to the Part-of-Speech Tagger Service</h1>
-            <p>Check out the <a href="/docs">docs</a> for more information</p>
+            <p>A microservice for tagging English text with part-of-speech tokens and lemmatisation.</p>
+            <p>Check out the <a href="/docs">docs</a> for more information.</p><br/>
             <p>Powered by
                 <a href="https://nestjs.com/">Nest</a>, and the
-                <a href="https://nlp.stanford.edu/software/tagger.shtml">Stanford Log-linear Part-Of-Speech Tagger</a>
+                <a href="https://nlp.stanford.edu/software/tagger.shtml">Stanford Log-linear Part-Of-Speech Tagger</a>.
             </p>
         `;
     }
