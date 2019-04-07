e701243b5c67db7e775c1b4a09d6939b8aca2701
Sam Hunt <s2112201@ipc.ac.nz>
2019-04-07 21:19:06 +1200

    docs: add initial swagger documentation at /docs

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+
+    const options = new DocumentBuilder()
+        .setTitle('Part-of-speech Tagger Service')
+        .setDescription(process.env.npm_package_description)
+        .setVersion(process.env.npm_package_version)
+        .addTag(process.env.npm_package_name)
+        .build();
+    const document = SwaggerModule.createDocument(app, options);
+    SwaggerModule.setup('docs', app, document);
+
     await app.listen(3000);
 }
 bootstrap();
