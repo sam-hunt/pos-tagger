633bf3f3ed45efe0e840001dabd799990cab44ba
Sam Hunt <samhunt.dev@gmail.com>
2019-05-26 23:55:24 +1200

    feat: Enable cors for frontend ajax requests

--- src/main.ts
+++ src/main.ts
@@ -15,6 +15,7 @@ async function bootstrap() {
     const document = SwaggerModule.createDocument(app, options);
     SwaggerModule.setup('docs', app, document);
 
+    app.enableCors();
     await app.listen(3001);
 }
 bootstrap();
